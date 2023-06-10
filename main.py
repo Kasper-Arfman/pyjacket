ae852ab4164666f37f247e959e3bbf80066428b7
Arfman <kasper.arfman@wur.nl>
2023-06-10 22:04:19 +0200

    update 23/10/06

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+# cv tools
+# color picker
+#  choose color using rgb sliders
+
+# get color by clicking an image
+
+
+# skeletonize
+# 
+
+
+
+
+
+
+
+
+
+# === 
+
+# manually select rois in an image
+
+# -> compute various parameters in time
+#   -> end-to-end (scalar)
+#   -> apparent contour length (scalar)
+#   -> SNR (scalar)
+#   -> intensity_profile x, y  of varying length per frame
+
+#  -> output results as file
---
+++ src/bioinformatics/todo.md
@@ -0,0 +1,9 @@
+
+
+
+
+write a short program that allows me to test whether a sequence is present in a protein
+
+
+
+
