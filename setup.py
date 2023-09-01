cd1221ea11b3a89619fe4465f2fae76793948881
Arfman <kasper.arfman@wur.nl>
2023-09-01 09:11:29 +0200

    manual versioning

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+
+__version__ = '0.1.1'
+
 GIT_USER = 'Kasper-Arfman'
 NAME = 'testpyjacket'
 
@@ -6,19 +9,19 @@ import setuptools
 import subprocess
 import os
 
-remote_version = (
-    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
-    .stdout.decode("utf-8")
-    .strip()
-)
+# remote_version = (
+#     subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+#     .stdout.decode("utf-8")
+#     .strip()
+# )
 
-if "-" in remote_version:
-    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
-    # pip has gotten strict with version numbers
-    # so change it to: "1.3.3+22.git.gdf81228"
-    # See: https://peps.python.org/pep-0440/#local-version-segments
-    v,i,s = remote_version.split("-")
-    remote_version = v + "+" + i + ".git." + s
+# if "-" in remote_version:
+#     # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
+#     # pip has gotten strict with version numbers
+#     # so change it to: "1.3.3+22.git.gdf81228"
+#     # See: https://peps.python.org/pep-0440/#local-version-segments
+#     v,i,s = remote_version.split("-")
+#     remote_version = v + "+" + i + ".git." + s
 
 # assert "-" not in remote_version
 # assert "." in remote_version
@@ -28,7 +31,7 @@ if "-" in remote_version:
 
 setuptools.setup(
     name=NAME,
-    version=remote_version,
+    version=__version__,
     author='Kasper Arfman',
     author_email='Kasper.arf@gmail.com',
     
