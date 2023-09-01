7321f533e25482c40eb0cdfe8f8c75c0437b6b45
Arfman <kasper.arfman@wur.nl>
2023-09-01 09:59:18 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,33 +1,9 @@
+import setuptools
 
-__version__ = '0.1.1'
+__version__ = '0.0.1'
 
 GIT_USER = 'Kasper-Arfman'
-NAME = 'testpyjacket'
-
-
-import setuptools
-import subprocess
-import os
-
-# remote_version = (
-#     subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
-#     .stdout.decode("utf-8")
-#     .strip()
-# )
-
-# if "-" in remote_version:
-#     # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
-#     # pip has gotten strict with version numbers
-#     # so change it to: "1.3.3+22.git.gdf81228"
-#     # See: https://peps.python.org/pep-0440/#local-version-segments
-#     v,i,s = remote_version.split("-")
-#     remote_version = v + "+" + i + ".git." + s
-
-# assert "-" not in remote_version
-# assert "." in remote_version
-# assert os.path.isfile("src/version.py")
-# with open("src/VERSION", "w", encoding="utf-8") as fh:
-    # fh.write("%s\n" % remote_version)
+NAME = 'pyjacket'
 
 setuptools.setup(
     name=NAME,
@@ -54,7 +30,7 @@ setuptools.setup(
     ],
     # python_requires="",
     # entry_points=[],
-    # install_requires=[],
+    install_requires=open('requirements.txt').read().split('\n')[:-1],
 
     # # Add _ prefix to the names of temporary build dirs
     # options={'build': {'build_base': '_build'}, },
