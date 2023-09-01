bd1efef469c1ee70ae2438940af01a51a0d06efe
Arfman <kasper.arfman@wur.nl>
2023-09-01 08:37:31 +0200

    steal setup from repo

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
-import subprocess
-import setuptools
-
 GIT_USER = 'Kasper-Arfman'
 NAME = 'pyjacket'
 
 
-remote_version = subprocess.run(
-    'git describe --tags'.split(),
-    stdout=subprocess.PIPE
-    ).stdout.decode('utf-8').strip().split('-')[0]
+import setuptools
+import subprocess
+import os
 
+remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
 
-# remote_version = '1.4.8'
+if "-" in remote_version:
+    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
+    # pip has gotten strict with version numbers
+    # so change it to: "1.3.3+22.git.gdf81228"
+    # See: https://peps.python.org/pep-0440/#local-version-segments
+    v,i,s = remote_version.split("-")
+    remote_version = v + "+" + i + ".git." + s
+
+assert "-" not in remote_version
+assert "." in remote_version
+
+assert os.path.isfile("cf_remote/version.py")
+with open("cf_remote/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % remote_version)
 
 setuptools.setup(
     name=NAME,
