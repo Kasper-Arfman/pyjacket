f6c7c21c10e7ed5319b718b2e4f9c8950d3d843e
Kasper Arfman <kasper.arf@gmail.com>
2024-04-26 19:21:51 +0200

    prime representation

--- pyjacket/ntheory/factors.py
+++ pyjacket/ntheory/factors.py
@@ -6,6 +6,30 @@ def factors(n):
     step = n%2 + 1
     return [[i, n//i] for i in range(1, isqrt(n)+1, step) if not n % i]
 
+from itertools import product
+def factors(pf: dict):
+    coefs = product(*[range(v//2+1) for v in pf.values()])
+    next(coefs)
+    for coef in coefs:
+        f1 = {p: c for p, c in zip(pf, coef)}
+        f2 = {p: n - c for (p, n), c in zip(pf.items(), coef)}
+        yield f1
+        if f1 != f2:
+            yield f2
+
+
+for f in factors({11:3}):
+    print(f)
+# q = product(*[])
+# q = list(q)
+# print(q)
+
+
+print('finished')
+exit()
+
+
+
 
 
 def divisors(n):
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 GIT_USER = 'Kasper-Arfman'
 NAME = 'pyjacket'
